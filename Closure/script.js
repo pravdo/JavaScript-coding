89d743feb2df47ade8c0c3c1cdab634d5e0b82c9
pravdo <40321240+pravdo@users.noreply.github.com>
2021-02-28 19:25:37 +0200

    Add files via upload

---
+++ Closure/script.js
@@ -0,0 +1,27 @@
+/*
+var passed = 3;
+
+var addTo = function(){
+    var inner = 2;
+    return passed + inner;
+};
+
+console.log(addTo());
+*/
+
+var addTo = function(passed){
+
+    var add = function(inner){
+        return passed + inner;
+    };
+    return add;
+};
+
+var addThree = new addTo(3);
+var addFour = new addTo(4);
+
+//console.dir(addThree);
+//console.dir(addFour);
+
+console.log(addThree(1));
+console.log(addFour(1));
